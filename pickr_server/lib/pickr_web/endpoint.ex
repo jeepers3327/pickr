2cd010319aa4e40cb2da72287931e552baafe7d2
Jhefrey Sajot <heifurukawa1997@gmail.com>
2020-11-09 20:13:19 +0800

    Add check origin for usersocket

--- pickr_server/lib/pickr_web/endpoint.ex
+++ pickr_server/lib/pickr_web/endpoint.ex
@@ -12,8 +12,7 @@ defmodule PickrWeb.Endpoint do
 
   socket "/socket", PickrWeb.UserSocket,
     websocket: true,
-    longpoll: false,
-    check_origin: ["https://pickr.vercel.app"]
+    longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
